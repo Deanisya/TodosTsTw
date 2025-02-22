983132d04dfd801d5af9f3c8225e9ea0025beced
Deanisya <vinogradova19.99@mail.ru>
2025-02-23 00:26:34 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import { dummyData } from './data/todos';
+
 function App() {
+	const [todos, setTodos] = useState(dummyData);
+
+	function setTodoCompleted(id: number, completed: boolean) {
+		setTodos(prevTodos => prevTodos.map(todo => (todo.id === id ? { ...todo, completed } : todo)));
+	}
+
 	return (
-		<>
-			<div className='bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5'>
-				<div>
-					<span className='inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg'>
-						<svg className='h-6 w-6 stroke-white'></svg>
-					</span>
+		<main className='py-10 h-screen space-y-5'>
+			<h1 className='text-3xl font-bold underline text-center'>Todo List 2.0</h1>
+			<div className='max-w-lg mx-auto bg-slate-100 rounded-mb p-5'>
+				<div className='space-y-2'>
+					{todos.map(todo => (
+						<TodoItem key={todo.id} todo={todo} onCompletedChange={setTodoCompleted} />
+					))}
 				</div>
-				<h3 className='text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight '>Writes upside-down</h3>
-				<p className='text-gray-500 dark:text-gray-400 mt-2 text-sm '>The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.</p>
 			</div>
-			<h1 className='text-3xl font-bold underline decoration-amber-500'>Hello world!</h1>
-		</>
+		</main>
 	);
 }
 
